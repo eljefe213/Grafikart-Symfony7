08ed0dba2618cd9c0246c55d424d0ec846f211aa
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-31 16:27:43 +0100

    WIP: Update the HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
     #[Route("/", name: "home")]
-    function index(): response
+    function index(Request $request): response
     {
-        return new response('Bonjour les gens !!');
+        return new response('Bonjour ' . $request->query->get('name', 'Inconnu'));
     }
 }
