fe9ac24c8ea03e1df19bd0ef3d0154992c5ecec3
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-31 16:54:35 +0100

    WIP: Add the show method & update the index method on the RecipeController

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -3,15 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
-    public function index(Request $request, string $slug, int $id): Response
+    #[Route('/recette', name: 'recipe.index')]
+    public function index(Request $request): Response
     {
-        dd($slug, $id);
+        return new Response('Recettes');
+    }
+    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+    public function show(Request $request, string $slug, int $id): Response
+    {
+
+        return $this->json([
+            'slug' => $slug
+        ]);
     }
 }
