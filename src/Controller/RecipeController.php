dbc46c7d841bb5dc87e7f8ab2d6454e2e35ab17c
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-31 16:47:18 +0100

    WIP: Update the index method of the RecipeController to add direcly the params  &

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -10,8 +10,8 @@ use Symfony\Component\Routing\Attribute\Route;
 class RecipeController extends AbstractController
 {
     #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
-    public function index(Request $request): Response
+    public function index(Request $request, string $slug, int $id): Response
     {
-        dd($request->attributes->get('slug'), $request->attributes->getInt('id'));
+        dd($slug, $id);
     }
 }
