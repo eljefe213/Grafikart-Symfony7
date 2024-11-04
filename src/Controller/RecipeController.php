046bd72135541f31e5cf88ecd9186f9c21612e94
abdelghani hamaz <abdelghani.hamaz.pro@gmail.com>
2024-11-04 06:51:40 +0100

    WIP: Update RecipeController to print all recipes from the Database

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,14 +11,18 @@ use Symfony\Component\Routing\Attribute\Route;
 class RecipeController extends AbstractController
 {
     #[Route('/recettes', name: 'recipe.index')]
-    public function index(Request $request): Response
+    public function index(Request $request, RecipeRepository $repository): Response
     {
-        return $this->render('recipe/index.html.twig');
+        $recipes = $repository->findAll();
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipes
+        ]);
     }
     #[Route('/recettes/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
-    public function show(Request $request, string $slug, int $id): Response
+    public function show(Request $request, string $slug, int $id, RecipeRepository $repository): Response
     {
-
+        $recipe = $repository->find($id);
+        dd($recipe);
         return $this->render('recipe/show.html.twig', [
             'slug' => $slug,
             'id' => $id
